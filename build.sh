64f61fd167da32bc385dd0395712c2147e6c473d
Zabrimus <github@hannebauer.org>
2022-04-08 18:12:22 +0200

    added cache

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+BRANCH=exp-vdr-opt
+
+RUNNER_BUILDDIR=build.CoreELEC-Amlogic-ng.arm-19
+RUNNER_CACHE=exp-vdr-opt-build
+RUNNER_SOURCES=exp-vdr-opt-sources
+
 # checkout / pull
 if [ ! -d CoreELEC ]; then
    git clone https://github.com/Zabrimus/CoreELEC.git
@@ -9,11 +15,19 @@ else
    git pull
 fi
 
-git checkout exp-vdr-opt
+git checkout ${BRANCH}
+
+# restore build-cache
+if [ -d ../../build-cache/${RUNNER_CACHE} ]; then
+   ln -s ../../build-cache/${RUNNER_CACHE} ${RUNNER_BUILDDIR}
+fi
+
+if [ -d ../../build-cache/${RUNNER_SOURCES} ]; then
+   ln -s ../../build-cache/${RUNNER_SOURCES} sources
+fi
 
 # build
-cd CoreELEC/
-rm target/*
+rm -f target/*
 make
 
 # Extract VDR archive
@@ -23,8 +37,8 @@ mkdir -p vdr-tar
 cp -a mnt/opt vdr-tar
 
 # Cleanup
-rm   vdr-tar/opt/.opt
-rm   vdr-tar/opt/vdr/bin/{createcats,cxxtools-config,gdk-*,iconv,pango*,rsvg-convert,tntnet-config,update-mime-database}
+rm vdr-tar/opt/.opt
+rm vdr-tar/opt/vdr/bin/{createcats,cxxtools-config,gdk-*,iconv,pango*,rsvg-convert,tntnet-config,update-mime-database}
 rm -Rf vdr-tar/opt/vdr/include
 rm -Rf vdr-tar/opt/vdr/lib/pkgconfig
 rm -Rf vdr-tar/opt/vdr/share/{doc,mime,pkgconfig,tntnet}
@@ -37,3 +51,19 @@ tar -czhf target/coreelec-vdr.tar.gz -C vdr-tar .
 umount mnt
 rmdir mnt
 rm -Rf vdr-tar
+
+# save build cache
+mkdir -p ../../build-cache
+
+if [ ! -d ../../build-cache/${RUNNER_CACHE} ]; then
+   mv ${RUNNER_BUILDDIR} ../../build-cache/${RUNNER_CACHE}
+else
+   rm ${RUNNER_BUILDDIR}
+fi
+
+if [ ! -d ../../build-cache/${RUNNER_SOURCES} ]; then
+   mv sources ../../build-cache/${RUNNER_SOURCES}
+else
+   rm sources
+fi
+
